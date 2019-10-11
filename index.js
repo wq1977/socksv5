b963db377d2f1ae6147d062bb4f396a6266425e0
wesleywang <9610848@qq.com>
2019-10-11 09:42:12 +0800

    make it like an ss server

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var fs = require('fs'),
     path = require('path');
 
-['server', 'client', 'Agents'].forEach(function(f) {
+['server', 'client', 'rserver', 'Agents'].forEach(function(f) {
   var exp = require(__dirname + '/lib/' + f),
       keys = Object.keys(exp);
   for (var i = 0, len = keys.length; i < len; ++i)
